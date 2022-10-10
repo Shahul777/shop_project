7bbe01b355bbd0dbde70f7a167e152c6ecfe4e6a
Shahul <rahima40273@gmail.com>
2022-10-10 14:47:29 +0530

    first commit

--- shop_project/urls.py
+++ shop_project/urls.py
@@ -19,13 +19,13 @@ from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import url
+# from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('kodambakkam/',include('shopAccountKdm.urls')),
     path('trustpuram/',include('shopAccountTpm.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 
 
